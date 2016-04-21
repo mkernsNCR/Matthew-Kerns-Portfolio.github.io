01e233a7f7392c4495060ab5fa7d6905abea701c
Matthew Lamar Kerns <matthewkerns@ymail.com>
2016-04-21 14:17:59 -0400

    Added new js plugin

--- js/script.js
+++ js/script.js
@@ -12,3 +12,17 @@ $(function() {
     }
   });
 });
+
+
+
+// Will revist this later
+
+// $(document).ready(function () {
+//   $("#fakeloader").fakeLoader({
+//     timeToHide:1200, //Time in milliseconds for fakeLoader disappear
+//     zIndex:999, // Default zIndex
+//     spinner:"spinner6",//Options: 'spinner1', 'spinner2', 'spinner3', 'spinner4', 'spinner5', 'spinner6', 'spinner7'
+//     bgColor:"#2ecc71", //Hex, RGB or RGBA colors
+//     //imagePath:"yourPath/customizedImage.gif" If you want can you insert your custom image
+//   });
+// });
